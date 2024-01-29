f3c6918fd8c00a03925f807b34d0e85bde1c46b7
Kostov Ilya <kostov.odessa21@gmail.com>
2024-01-30 00:56:43 +0200

    Correcting Code

---
+++ Russian roulette.py
@@ -0,0 +1,96 @@
+import random
+import time
+def func():
+    print("---------------------------")
+    print("1 раунд")
+    time.sleep(3)
+    print("---------------------------")
+    print("2 холостых - 3 заряженых")
+    time.sleep(4)
+    print("Шанс выпадения холостого составляет 40% | Шанс выпадения заряженого составляет 60%")
+    time.sleep(5)
+    n_1 = random.randrange(1,6)
+    if(n_1 < 4):
+        print("Вам выпал заряжений!")
+        time.sleep(3)
+        print("Вы погибли(")
+        x = str(input("Вы хотите начать новую игру Y/N: "))
+        print(x)
+        if(x=="Y"):
+            print("Колесо Сансары снова вращается")
+            time.sleep(3)
+            func()
+        elif(x=="N"):
+            print("Хорошо!")
+        else:
+            print("Ошибка!")
+            time.sleep(1)
+            print("Перезагрузка Игры.")
+            time.sleep(1.5)
+            print("Перезагрузка Игры..")
+            time.sleep(2)
+            print("Перезагрузка Игры...")
+            time.sleep(2.5)
+            print("Перезагрузка Игры....")
+            time.sleep(3)
+            func()
+    elif(n_1 == 4,5):
+        print("Вам выпал холостой! Вам повезло...")
+        time.sleep(2.5)
+        print("Вы проходите на следующий раунд")
+        m = str(input("Нажмите Y для продолжения игры: "))
+        if(m=="Y"):
+            print(
+
+                # Empty
+            )
+            print("---------------------------")
+            print("2 раунд")
+            time.sleep(3)
+            print("---------------------------")
+            print("3 холостых - 3 заряженых")
+            time.sleep(4)
+            print("Шанс выпадения холостого составляет 50% | Шанс выпадения заряженого составляет 50%")
+            time.sleep(5)
+            p_1 = random.randrange(1,7)
+            if(p_1 == 4,5,6):
+                print("Вам выпал заряжений!")
+                time.sleep(3)
+                print("Вы погибли(")
+                x_1 = str(input("Вы хотите начать новую игру Y/N: "))
+                print(x_1)
+                if(x_1=="Y"):
+                    print("Колесо Сансары снова вращается")
+                    time.sleep(3)
+                    func()
+                elif(x_1=="N"):
+                    print("Хорошо!")
+                else:
+                    print("Ошибка!")
+                    time.sleep(1)
+                    print("Перезагрузка Игры.")
+                    time.sleep(1.5)
+                    print("Перезагрузка Игры..")
+                    time.sleep(2)
+                    print("Перезагрузка Игры...")
+                    time.sleep(2.5)
+                    print("Перезагрузка Игры....")
+                    time.sleep(3)
+                    func()
+            elif(p_1 < 4):
+                print("Вам выпал холостой! Вам повезло...")
+                time.sleep(2.5)
+                print("Вы проходите на следующий раунд")
+                print(
+
+                    # Empty
+                )
+            else:
+                print("Ошибка!")
+                func()
+            print("---------------------------")
+    else:
+        print("Ошибка!")
+        func()
+    print("---------------------------")
+func()
